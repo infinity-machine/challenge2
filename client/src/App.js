cf3cf59107a329386a4d7ca38188de106256f641
Connor Carciofini <ccarciofini@gmail.com>
2022-09-13 12:32:27 -0500

    working

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,78 @@
-import React from 'react'
-import './index.css'
-import Header from './components/Header'
-import Card from './components/Card'
-import Main from './pages/Main'
-import Resume from './pages/Resume'
+import React from 'react';
+import './index.css';
+import Header from './components/Header';
+import Card from './components/Card';
+import Main from './pages/Main';
+import Resume from './pages/Resume';
 import Portfolio from './pages/Portfolio';
 import Media from './pages/Media'
 import Contact from './pages/Contact';
 import { Routes, Route } from 'react-router-dom';
+import * as THREE from 'three';
 
 function App() {
+  let scene, camera, renderer, stars, starGeo;
+
+    function init() {
+
+      scene = new THREE.Scene();
+
+      camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+      camera.position.z = 1;
+      camera.rotation.x = Math.PI/2;
+
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(renderer.domElement);
+
+      starGeo = new THREE.SphereGeometry( 15, 32, 16 );
+      for(let i=0;i<6000;i++) {
+        star = new THREE.Vector3(
+          Math.random() * 600 - 300,
+          Math.random() * 600 - 300,
+          Math.random() * 600 - 300
+        );
+        star.velocity = 0;
+        star.acceleration = 0.01;
+        starGeo.vertices.push(star);
+      }
+
+      let sprite = new THREE.TextureLoader().load( 'star.png' );
+      let starMaterial = new THREE.PointsMaterial({
+        color: 0xaaaaaa,
+        size: 0.7,
+        map: sprite
+      });
+
+      stars = new THREE.Points(starGeo,starMaterial);
+      scene.add(stars);
+
+      window.addEventListener("resize", onWindowResize, false);
+
+      animate(); 
+    }
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+      }
+    function animate() {
+      // starGeo.vertices.forEach(p => {
+      //   p.velocity += p.acceleration
+      //   p.y -= p.velocity;
+        
+      //   if (p.y < -200) {
+      //     p.y = 200;
+      //     p.velocity = 0;
+      //   }
+      // });
+      starGeo.verticesNeedUpdate = true;
+      stars.rotation.y +=0.002;
+    
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    }
+    init();
   return (
     <div>
       < Header />
