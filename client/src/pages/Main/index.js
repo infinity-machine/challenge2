38b47310e087a4245d8f3c29172012061bb53f58
Connor Carciofini <ccarciofini@gmail.com>
2022-09-13 15:51:36 -0500

    tweaks

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -4,7 +4,7 @@ import Card from '../../components/Card'
 
 const Main = () => {
     return (
-        <></>
+        <h3 className="centertext">FINDING CREATIVE SOLUTIONS FROM MINNEAPOLIS TO THE METAVERSE</h3>
     )
 }
 
